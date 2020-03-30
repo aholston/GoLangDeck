e4483e1fb7597900df76341693fae8464a11e417
Andre Holston <andre@koddi.com>
2020-03-30 15:07:31 -0500

    Randomize deck shuffle better

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 7)
-
-	// hand.print()
-	// remainingDeck.print()
-
 	cards := getFromFile("My Cards")
-	fmt.Println(cards)
+	cards.shuffle()
+	cards.print()
 }
